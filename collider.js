6d7ed9a7f55fbffda3a29687b8605eacc2611b44
Hack Reactor Students <github@hackreactor.com>
2013-08-17 16:46:56 -0700

    collision detector working

--- collider.js
+++ collider.js
@@ -1,17 +1,19 @@
+var SIZE = 800;
+
 var gameOptions = {
-  height: 450,
-  width: 700,
-  nEnemies: 10,
+  height: SIZE,
+  width: SIZE,
+  nEnemies: 30,
   padding: 20
 };
-
+var scoreBoard = {};
 var gameBoard = d3.select('.container').append("svg:svg")
                     .attr('width',gameOptions.width)
                     .attr('height',gameOptions.height);
 
 //genereate random coordiantes function
 var newCoordinates = function(){
-  return Math.random() *400;
+  return Math.random() *SIZE;
 };
 
 var createEnemies = function(){
@@ -30,11 +32,52 @@ var createPlayer = function(){
   Player.identity = [];
   Player.identity.push({
     id: 'player',
-    x: 100,
-    y: 100
+    x: SIZE / 2,
+    y: SIZE / 2
   });
   return Player.identity;
 };
+var collision = function(){
+};
+//collision detection
+var collisionCheck = function(enemy, callback){
+  var radiusSum = parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
+  var xDiff = parseFloat(enemy.attr('cx')) - player.attr('cx');
+  var yDiff = parseFloat(enemy.attr('cy')) - player.attr('cy');
+
+  var seperation = Math.sqrt(Math.pow(xDiff,2) + Math.pow(yDiff,2));
+  if(radiusSum > seperation){
+    return callback(player, enemy);
+  }
+};
+
+//tweenfunction
+var tweenCollisionCheck = function(endPointData){
+  var enemy = d3.select(this);
+
+  var startPoint = {
+    x: parseFloat(enemy.attr('cx')),
+    y: parseFloat(enemy.attr('cy'))
+  };
+
+  var endPoint = {
+    x: endPointData.x,
+    y: endPointData.y
+  };
+
+  return function(t){
+    collisionCheck(enemy, collision);
+    var enemyNextPosition = {
+      x: startPoint.x + (endPoint.x - startPoint.x)*t,
+      y: startPoint.y + (endPoint.y - startPoint.y)*t
+    };
+
+    enemy.attr('cx', enemyNextPosition.x)
+          .attr('cy', enemyNextPosition.y);
+  };
+};
+
+
 
 var allEnemies = gameBoard.selectAll('svg').data(createEnemies);
 var player = gameBoard.selectAll('svg').data(createPlayer);
@@ -54,21 +97,22 @@ player.enter().append('svg:circle')
             .attr('cy', function(d){return d.y})
             .attr('r', '10')
             .attr('fill', 'orange')
-            .call(d3.behavior.drag().on('drag', function(){moveP()}));
+            .call(d3.behavior.drag().on('drag', function(d,i){moveP(d)}));
 
-var moveP = function(){
+var moveP = function(d){
   player.attr('cx', function(){return d3.event.dx + parseInt(player.attr('cx'))})
         .attr('cy', function(){return d3.event.dy + parseInt(player.attr('cy'))});
 };
 
-var MOVE = function (){
+var moveStep = function (){
   window.setInterval(moveEnemies, 1000);
 };
 
 var moveEnemies = function() {
   allEnemies.transition().duration(1000)
             .attr('cx', function(d){d.x = newCoordinates(); return d.x})
-            .attr('cy', function(d){d.y = newCoordinates(); return d.y});
+            .attr('cy', function(d){d.y = newCoordinates(); return d.y})
+            .tween('custom', tweenCollisionCheck);
 };
 
-MOVE();
+moveStep();
